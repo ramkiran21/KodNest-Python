74c21e3bd3c1cabcb5c5b2b550120b74ead4ed56
R kiran <ramkiran9743@gmail.com>
2026-08-04 17:04:52 +0530

    day 2 tasks

---
+++ track/M02/T01/src/day2.py
@@ -0,0 +1,51 @@
+#program4
+# Add two numbers
+
+# Read the numbers
+first_number = int(input())
+second_number = int(input())
+# Calculate the sum
+sum_result = first_number + second_number
+
+# Display the result
+print(f"Sum: {sum_result}")
+
+
+#program3
+#Calculate Division and Floor Division
+first_number = int(input())
+second_number = int(input())
+
+# Write your code here
+print(first_number / second_number)
+print(first_number // second_number )
+
+
+#program2
+#Fix the Student Age Calculation
+
+current_age = "21"
+next_age = int(current_age) + 1
+
+print(f"Age next year: {next_age}")
+
+
+#program 1
+#Convert Student Registration Values
+
+age_text = "21"
+course_fee_text = "499.50"
+attempts = 3
+
+# Convert the values here
+age_text = int(age_text)
+course_fee_text = float(course_fee_text)
+attempts = str(attempts)
+
+# Display the converted values and their data types
+print(age_text)
+print(type(age_text))
+print(course_fee_text)
+print(type(course_fee_text))
+print(attempts)
+print(type(attempts))
